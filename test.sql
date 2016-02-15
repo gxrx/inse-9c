0a525ce7f7a6665f05f082086b4992ac0ee650aa
gxrx <up616531@myport.ac.uk>
2016-02-15 19:23:38 +0000

    Updated
    Test

--- test.sql
+++ test.sql
@@ -3,3 +3,5 @@ GRANT SELECT (ID, First_Name, Last_Name) ON T_X.Student TO 'sign';
 GRANT SELECT (ID, Name) ON T_X.Unit TO 'sign';
 GRANT SELECT (U_ID, S_ID, Mark) ON T_X.Enrolment TO 'sign';
 GRANT SHOW VIEW, SELECT ON T_X.PassedStudents TO 'sign';
+
+Changes
