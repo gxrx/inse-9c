22eaba119b20e39926feabca2039679476330a17
WaltersD96 <up725137@myport.ac.uk>
2016-02-29 19:17:10 +0000

    Update Main.java
    Added Testing for Project.java
    to note: testing is currently commented.

--- Event_Handling/Main.java
+++ Event_Handling/Main.java
@@ -14,5 +14,19 @@ public class Main {
         {
             System.out.println("Event Name: " + e.name + " Year: " + e.eStart.get(1) + " Month: " + e.eStart.get(2) + " Day: " + e.eStart.get(5));
         }
+     /* Create a Project - Add Events - Print Event Data.
+        System.out.println("TESTING PROJECT");
+        Project project1 = new Project("project1", p ,y);
+        Event event1 = new Event("Event 1", p, y, "Notes", true, 0, 1);
+        Event event2 = new Event("Event 2", p, y, "More Notes", false, 1, 1);
+        project1.addEvent(event1);
+        project1.addEvent(event2);
+        System.out.println(project1.getSize());
+        project1.getEventList().stream().forEach((e) -> {
+            System.out.println("Event Name: " + e.name + " Year: " + e.eStart.get(1) + " Month: " + e.eStart.get(2) + " Day: " + e.eStart.get(5));
+        }); 
+        */
+    }
+    
     }
 }
