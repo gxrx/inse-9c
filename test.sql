a879be7f917318cfc3d246c5e60b6577bd6b13f3
gxrx <up616531@myport.ac.uk>
2016-02-29 19:40:49 +0000

    Removed Test

--- test.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'sign' IDENTIFIED BY 'sign';
-GRANT SELECT (ID, First_Name, Last_Name) ON T_X.Student TO 'sign';
-GRANT SELECT (ID, Name) ON T_X.Unit TO 'sign';
-GRANT SELECT (U_ID, S_ID, Mark) ON T_X.Enrolment TO 'sign';
-GRANT SHOW VIEW, SELECT ON T_X.PassedStudents TO 'sign';
-
-Changes
