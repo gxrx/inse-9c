8653814df56d0ee7f6bcb794bc99696c6aca5730
garethallenstringer <up659679@myport.ac.uk>
2016-02-29 19:59:04 +0000

    Added FXML Controllers

---
+++ projectChartr/src/projectchartr/NewProjectSceneController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectchartr;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author GarethAllenStringer
+ */
+public class NewProjectSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
