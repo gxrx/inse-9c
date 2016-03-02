49bdd31108ffddfbdb54c51ac7d07ccc0575bf5f
WaltersD96 <up725137@myport.ac.uk>
2016-03-02 19:33:17 +0000

    Update Project.java

--- Event_Handling/Project.java
+++ Event_Handling/Project.java
@@ -13,7 +13,7 @@ public class Project {
     public Project(String na, int[] st, int[] en) {
         eStart = Calendar.getInstance();
         eEnd = Calendar.getInstance();
-        eStart.set(st[0], st[1], st[2]); 
+        eStart.set(st[0], st[1], st[2]);
         eEnd.set(en[0], en[1], en[2]);
         name = na;
 
@@ -35,36 +35,29 @@ public class Project {
     public int getSize() {
         return eventList.size();
     }
-    
-    public ArrayList<Event> getEventList()
-    {
+
+    public ArrayList<Event> getEventList() {
         return eventList;
     }
-    
-     public void addEvent(double inputID){
-        checkDuplicate(inputID);
-        //add the event
-    }
-   
-    //checks for a duplcate Event ID. returns false if there is. 
-    public boolean checkDuplicate(double inputID){
-        for(Event e: eventList)
+
+    public void addEvent(double inputID, Event event) {
+        if (checkDuplicate(inputID) == false)
         {
-            if (inputID == e.getID())
-            {
-            return false;
+           eventList.add(event); 
+        }
+
+    }
+
+    //checks for a duplcate Event ID. returns false if there is. 
+    public boolean checkDuplicate(double inputID) {
+        String stringInputID = String.valueOf(inputID);
+        String eventlistID = "";
+        for (Event e : eventList) {
+            eventlistID = String.valueOf(e.getID());
+            if (stringInputID.equals(eventlistID)) {
+                return false;
             }
         }
-            return true;
-        } 
-    
-    
-    
+        return true;
     }
-    
-    
-    
-    
-
-
-
+}
